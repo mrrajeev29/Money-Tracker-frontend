c98850de6f0bf23e8e2b060033ad18c8c2708411
Rajeev Ranjan <97228502+mrrajeev29@users.noreply.github.com>
2024-01-20 12:15:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Lock from './component/lock';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Lock/>,
+  },
+  {
+    path: "sub",
+    element: <App/>,
+  },
+  
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
